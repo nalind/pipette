f0610696263f319df67b1a6f36a4447ff5605e1c
Nalin Dahyabhai <nalin@redhat.com>
2020-09-13 04:37:59 -0400

    wip
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- pipette.sh
+++ pipette.sh
@@ -10,22 +10,25 @@ REPOSITORY=${REPOSITORY:-https://github.com/containers/buildah}
 VERSION=${VERSION:-v1.16.1}
 DOCKERFILE=contrib/buildahimage/stable/Dockerfile
 mkdir -p build/context build/output
-podman run --rm -it -v $(pwd)/build/context:/buildcontext:Z ${IMAGE} git clone ${REPOSITORY} /buildcontext
-podman run --rm -it -v $(pwd)/build/context:/buildcontext:Z ${IMAGE} sh -c "cd /buildcontext; git checkout ${VERSION}"
+podman run --rm -it -v $(pwd)/build/context:/buildcontext:z ${IMAGE} git clone ${REPOSITORY} /buildcontext
+podman run --rm -it -v $(pwd)/build/context:/buildcontext:z ${IMAGE} sh -c "cd /buildcontext; git checkout ${VERSION}"
 
 # Build the per-arch images.
 : > build/cid.txt
-for arch in ${ARCH:-x86_64}; do
+for arch in ${ARCH:-aarch64 ppc64le s390x x86_64}; do
 	mkdir -p build/output/${arch}/image
 	if test -s build/cid-${arch}.txt ; then
 		cat build/cid-${arch}.txt >> build/cid.txt
 		continue
 	fi
-	podman run --rm -d --cidfile build/cid-${arch}.txt -v $(pwd)/build/context:/buildcontext:Z -v $(pwd)/build/output/${arch}:/buildoutput:Z -e ARCH="${arch}" -e DOCKERFILE="${DOCKERFILE}" ${IMAGE} /pipette-arch.sh
+	podman run -d --cidfile build/cid-${arch}.txt -v $(pwd)/build/context:/buildcontext:z -v $(pwd)/build/output/${arch}:/buildoutput:z -e ARCH="${arch}" -e DOCKERFILE="${DOCKERFILE}" ${IMAGE} /pipette-arch.sh
 	cat build/cid-${arch}.txt >> build/cid.txt
+	echo " "                  >> build/cid.txt
 done
-podman wait $(cat build/cid.txt)
+for cid in $(cat build/cid.txt) ; do
+	podman wait $cid
+done
 
 # Build the multi-arch image using the per-arch images.
-podman run --rm -it -v $(pwd)/build/output:/buildoutput:Z ${IMAGE} /pipette-list.sh
+podman run --rm -it -v $(pwd)/build/output:/buildoutput:z ${IMAGE} /pipette-list.sh
 skopeo inspect --raw dir:build/list | jq
